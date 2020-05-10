7d0d43301c45739128ea0aa3609f7a0d18896753
Danylo <danillazorenko12@gmail.com>
2020-05-11 01:41:23 +0300

    change width

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         nextArrow:  '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 1024,
                 settings: {
                   dots: true,
                   arrows: false
