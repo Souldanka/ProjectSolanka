d8947a12da3b666384fc2f46119232baacda7f6b
Danylo <danillazorenko12@gmail.com>
2020-05-18 03:41:55 +0300

    add windovs ad validation:

--- src/js/script.js
+++ src/js/script.js
@@ -33,26 +33,21 @@ function toggleSlide(item) {
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
 
+ //Modal
+ $('[data-modal=consultation]').on('click', function() {
+  $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function() {
+  $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
 
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                     dots: true,
-//                     arrows: false
-//                 }
-//             }
-//         ]
-//     });
-    
-//     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-//         $(this)
-//           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-//           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-//     });
+$('.button_mini').each(function(i) {
+  $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+  })
+});
+
+$('#consultation-form').validate();
+$('#consultation form').validate();
+$('#order form').validate();
